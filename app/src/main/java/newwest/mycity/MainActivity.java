b7993a783ac967d2230ee3fd8edc0b24a3658d21
Sardar Ali <sali@blackducksoftware.com>
2017-02-25 13:08:53 -0800

    buttons and text boxes added

--- app/src/main/java/newwest/mycity/MainActivity.java
+++ app/src/main/java/newwest/mycity/MainActivity.java
@@ -1,13 +1,17 @@
 package newwest.mycity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +22,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView text_logout = (TextView) findViewById(R.id.text_logout);
+        text_logout.setTextColor(Color.RED);
+        text_logout.setTypeface(null, Typeface.BOLD);
+
+
     }
 
     @Override
