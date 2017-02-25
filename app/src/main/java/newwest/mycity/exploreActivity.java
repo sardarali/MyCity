b4ca0508fb49a1243c0a31aced04f1bcf83f263d
Yiming (Nathan) Zhang <yzhang@blackducksoftware.com>
2017-02-25 15:37:46 -0800

    rewards_page

--- app/src/main/java/newwest/mycity/exploreActivity.java
+++ app/src/main/java/newwest/mycity/exploreActivity.java
@@ -1,13 +1,34 @@
 package newwest.mycity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class exploreActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+        findViewById(R.id.Gobtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                go_rewards();
+            }
+        });
+    }
+    public void go_rewards() {
+//        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        // interval of alarm ring
+//        int interval = 8000; // in milliseconds
+
+        Intent i = new Intent(exploreActivity.this, RewardsActivity.class);
+        startActivity(i);
+        //finish();
     }
 }
