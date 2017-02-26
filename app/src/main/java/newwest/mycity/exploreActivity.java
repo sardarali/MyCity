4d70f86190388606706603a273841f1313b93a64
Sardar Ali <sali@blackducksoftware.com>
2017-02-25 20:03:19 -0800

    random park load added

--- app/src/main/java/newwest/mycity/exploreActivity.java
+++ app/src/main/java/newwest/mycity/exploreActivity.java
@@ -1,14 +1,16 @@
 package newwest.mycity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 public class exploreActivity extends AppCompatActivity {
     @Override
@@ -21,6 +23,41 @@ public class exploreActivity extends AppCompatActivity {
                 go_rewards();
             }
         });
+        findViewById(R.id.Searchbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                load_activity(v);
+            }
+        });
+    }
+
+    public void load_activity(View v){
+        TextView t = (TextView) findViewById(R.id.parkName);
+
+        int parksDataSize = MainActivity.parksDataset.size();
+        Random rand = new Random();
+        int  randomPark = rand.nextInt(parksDataSize+1);
+        ParkDataRow pdr = MainActivity.parksDataset.get(randomPark);
+
+
+        try
+        {
+            // get input stream
+            InputStream ims = getAssets().open("ab.png");
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            // set image to ImageView
+            ImageView iw = (ImageView) findViewById(R.id.parkImage);
+            iw.setImageDrawable(d);
+            ims .close();
+        }
+        catch(IOException ex)
+        {
+            //return;
+        }
+
+
+        t.setText(pdr.getParkName());
     }
     public void go_rewards() {
         Intent i = new Intent(exploreActivity.this, RewardsActivity.class);
