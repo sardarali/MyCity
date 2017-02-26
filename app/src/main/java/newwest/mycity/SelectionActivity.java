ed42cb918411df0abd8bef7ecba7fbc9a66eb025
Jongrin Kim <jkim@blackducksoftware.com>
2017-02-26 12:48:43 -0800

    quest Treasury page added

--- app/src/main/java/newwest/mycity/SelectionActivity.java
+++ app/src/main/java/newwest/mycity/SelectionActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 
@@ -80,5 +82,34 @@ public class SelectionActivity extends AppCompatActivity {
         startActivity(i);
         //finish();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.QuestedTreasure) {
+            Intent i = new Intent(SelectionActivity.this, SettingActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
